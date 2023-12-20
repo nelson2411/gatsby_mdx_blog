f50f4ac84cea3a79b3551aec43fb82839f97a5a6
Nelson Rosales <nelson.rosales83@gmail.com>
2023-12-20 12:06:24 +0100

    mdx pluging added

--- src/pages/newsletter.js
+++ src/pages/newsletter.js
@@ -13,6 +13,7 @@ const NewsLetter = () => {
             method="post"
             data-netlify="true"
             data-netlify-honeypot="bot-field"
+            action="/success"
           >
             <input type="hidden" name="form-name" value="testing-contact" />
             <input
